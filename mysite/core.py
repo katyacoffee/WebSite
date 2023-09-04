213a7c149f9bb8e1091afb47bc34ca732d51abf6
Екатерина Козакова <ekaterinakozakova@Air-Ekaterina.fritz.box>
2023-09-04 08:54:39 +0300

    fix k-index

--- mysite/core.py
+++ mysite/core.py
@@ -21,7 +21,7 @@ base_dir_serv_vlf = '~mikhnevo/metronix/METRONIX_SDVamp'
 base_dir_serv_tec = '~madrigal/IMG/WorldPlotAnim'
 base_dir_serv_gps = '~mikhnevo/gnss/tec_rot/prego/png' #!!
 base_dir_serv_lem = '~mikhnevo/LEMI018/PNG'
-base_dir_serv_k_ind = '~mikhnevo/K-INDEX'
+base_dir_serv_k_ind = '~mikhnevo/K-INDEX/PNG'
 source_vlf = 'vlf'
 source_tec = 'tec'
 source_gps = 'gps'
@@ -85,6 +85,7 @@ def get_k_ind_name(pic: str) -> str:
     return ''
 
 
+# ВОЗМОЖНО ЭТО ПРИГОДИТСЯ ДЛЯ k-index
 def get_img_k_index(img_list: list[str], year, mon) -> list[str]:
     img_list_k = []
     for im in img_list:
@@ -96,19 +97,26 @@ def get_img_k_index(img_list: list[str], year, mon) -> list[str]:
     return img_list_k
 
 
-def get_img_k_index_for_day(img_list: list[str], year, mon, day) -> list[str]:
-    img_list_k = []
-    i = 0
-    for im in img_list:
-        s = im.split('_')
-        if len(s) < 5:
-            continue
-        if int(s[2]) == int(year) and int(s[3]) == int(mon) and int(s[4]) == int(day):
-            #img_list_k.append(im)
-            #TODO: помещаем две предыдущих из img_list + im + две следующих из img_list в список img_list_k
-            # и тут же делаем return img_list_k
-        i += 1
-    return img_list_k
+# ВОЗМОЖНО ЭТО ПРИГОДИТСЯ ДЛЯ k-index
+# def get_img_k_index_for_day(img_list: list[str], year, mon, day) -> list[str]:
+#     img_list_k = []
+#     i = 0
+#     for im in img_list:
+#         s = im.split('_')
+#         if len(s) < 5:
+#             continue
+#         if int(s[2]) == int(year) and int(s[3]) == int(mon) and int(s[4]) == int(day):
+#             img_list_k.append(img_list[i-4])
+#             img_list_k.append(img_list[i-2])
+#             img_list_k.append(im)
+#             img_list_k.append(img_list[i+2])
+#             img_list_k.append(img_list[i+4])
+#             print(img_list_k)
+#             return img_list_k
+#             # помещаем две предыдущих из img_list + im + две следующих из img_list в список img_list_k
+#             # и тут же делаем return img_list_k
+#         i += 1
+#     return img_list_k
 
 
 def compare(st: str) -> str:
@@ -156,10 +164,10 @@ def get_img_list(yr: str, mon: str, day: str, source: str) -> list[str]:
     if source == source_vlf or source == source_lem:
         data_path = base_dir_serv + '/' + yr + '/' + mon + '/' + day
         print(data_path)
-    elif source == source_tec or source == source_gps:
+    elif source == source_tec or source == source_gps or source == source_k_ind:
         data_path = base_dir_serv + '/' + yr + '/' + mon
-    elif source == source_k_ind:
-        data_path = base_dir_serv
+    # elif source == source_k_ind:
+    #     data_path = base_dir_serv
         # print(data_path)
 
     # print(data_path)
@@ -188,22 +196,33 @@ def get_img_list(yr: str, mon: str, day: str, source: str) -> list[str]:
                     img_list1.append(pic)
                     img_list1.sort()
             img_list = img_list1
+        # elif source == source_k_ind:
+        #     img_list = get_img_k_index_for_day(img_list, yr, mon, day)  # TODO: тут надо брать из get_img_k_index_for_day()
+        #     # и no_data делать True только если img_list пустой
+        #     if len(img_list) == 0:
+        #         no_data = True
+        #
+        #     # for pic in img_list: # TODO: убрать
+        #     #     if get_day_from_k_pic(pic) == int(day):
+        #     #         img_list = [pic]
+        #     #         no_data = False
+        #     #         break
+        #     else: # TODO: а тут наоборот условие
+        #         no_data = False
+        #     print("imglist", img_list)
+        # elif source == source_tec:
+        #     no_data = True
+        #     for pic in img_list:
+        #         if get_day_from_tec_pic(pic) == int(day):
+        #             img_list = [pic]
+        #             no_data = False
+        #             break
+        #     if no_data:
+        #         img_list = []
         elif source == source_k_ind:
-            img_list = get_img_k_index(img_list, yr, mon) #TODO: тут надо брать из get_img_k_index_for_day()
-                                                          # и no_data делать True только если img_list пустой
-            no_data = True
-            for pic in img_list: # TODO: убрать
-                if get_day_from_k_pic(pic) == int(day):
-                    img_list = [pic]
-                    no_data = False
-                    break
-            if no_data: # TODO: а тут наоборот условие
-                img_list = []
-            print("imglist", img_list)
-        elif source == source_tec:
             no_data = True
             for pic in img_list:
-                if get_day_from_tec_pic(pic) == int(day):
+                if get_day_from_k_pic(pic) == int(day):
                     img_list = [pic]
                     no_data = False
                     break
@@ -250,10 +269,10 @@ def get_available_days(year: str, mon: str, source: str) -> list[int]:
             # TODO для gps!
             if source == source_vlf or source == source_lem:
                 data_path = base_dir_serv + '/' + year + '/' + mon + '/' + day
-            elif source == source_tec or source == source_gps:
+            elif source == source_tec or source == source_gps or source == source_k_ind:
                 data_path = base_dir_serv + '/' + year + '/' + mon
-            elif source == source_k_ind:
-                data_path = base_dir_serv
+            # elif source == source_k_ind:
+            #     data_path = base_dir_serv
             # elif source == source_gps:
             #     data_path = base_dir_serv + '/' + year + '/' + mon
             site = 'https://' + server_dir + '/' + data_path
@@ -268,8 +287,8 @@ def get_available_days(year: str, mon: str, source: str) -> list[int]:
                     r = s.post(site, data=payload)
                     # print(r.text)
                 resp = s.get(site, timeout=2)
-                if i == 1: #TODO remove!
-                    print(resp.text)
+                # if i == 1: #TODO remove!
+                print(resp.text)
                 img_list = get_images(resp)
                 if source == source_vlf:
                     img_list.sort(key=lambda pic: compare(get_station_name(pic)))
@@ -278,8 +297,6 @@ def get_available_days(year: str, mon: str, source: str) -> list[int]:
                     print(img_list)
                 # TODO для LEMI
                 elif source == source_k_ind:
-                    print("imglist", img_list)
-                    img_list = get_img_k_index(img_list, year, mon)
                     no_data = True
                     for pic in img_list:
                         if get_day_from_k_pic(pic) == int(day):
@@ -288,6 +305,17 @@ def get_available_days(year: str, mon: str, source: str) -> list[int]:
                             break
                     if no_data:
                         img_list = []
+                # elif source == source_k_ind:
+                #     print("imglist", img_list)
+                #     img_list = get_img_k_index(img_list, year, mon)
+                #     no_data = True
+                #     for pic in img_list:
+                #         if get_day_from_k_pic(pic) == int(day):
+                #             img_list = [pic]
+                #             no_data = False
+                #             break
+                #     if no_data:
+                #         img_list = []
                     print("imglist2", img_list)
                 elif source == source_tec:
                     no_data = True
